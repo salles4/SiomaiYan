544b67841b3bb90299dc8dfaf16c5a3de0cc4876
salles4 <fjsalles.04@gmail.com>
2024-02-17 21:27:56 +0800

    no aspect Ratio chart

--- chart1.js
+++ chart1.js
@@ -1,26 +1,30 @@
 
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ];
+const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+];
 
-    let ch = new Chart(
-        document.getElementById('revenue'),
-        {
-            type: 'line',
-            data: {
-                labels: data.map(row => row.year),
-                datasets: [
-                    {
-                        label: 'Revenue',
-                        data: data.map(row => row.count)
-                    }
-                ]
-            }
+let ch = new Chart(
+    document.getElementById('revenue'),
+    {
+        type: 'line',
+        data: {
+            labels: data.map(row => row.year),
+            datasets: [
+                {
+                    label: 'Revenue',
+                    data: data.map(row => row.count)
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
         }
-    );
+    }
+);
