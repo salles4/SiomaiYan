8520bab1edf77af3833b7c4b6da38bc97f8d0d6d
salles4 <fjsalles.04@gmail.com>
2024-02-17 20:55:55 +0800

    line

--- chart1.js
+++ chart1.js
@@ -12,50 +12,15 @@
     let ch = new Chart(
         document.getElementById('revenue'),
         {
-            type: 'bar',
+            type: 'line',
             data: {
                 labels: data.map(row => row.year),
                 datasets: [
                     {
-                        label: 'Acquisitions by year',
+                        label: 'Revenue',
                         data: data.map(row => row.count)
                     }
                 ]
             }
         }
     );
-
-
-
-
-// Load the Visualization API and the corechart package.
-google.charts.load('current', { 'packages': ['corechart'] });
-
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart() {
-
-    // Create the data table.
-    var data = google.visualization.arrayToDataTable([
-        ['Year', 'Sales', 'Expenses'],
-        ['2004', 1000, 400],
-        ['2005', 1170, 460],
-        ['2006', 660, 1120],
-        ['2007', 1030, 540]
-    ]);
-
-    // Set chart options
-    var options = {
-        'title': 'How Much Pizza I Ate Last Night',
-        curveType: 'function',
-        'height': 900
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-}
